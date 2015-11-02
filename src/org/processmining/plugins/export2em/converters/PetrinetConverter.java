31925d6c5f5236bb4925d343cfcfa1f9943e91c3
Tobias Potthoff <tp@tobiass-mbp.speedport_w_724v_09011603_00_012>
2015-11-02 12:06:54 +0100

    Hash Integers for IDs

--- src/org/processmining/plugins/export2em/converters/PetrinetConverter.java
+++ src/org/processmining/plugins/export2em/converters/PetrinetConverter.java
@@ -23,17 +23,17 @@ public class PetrinetConverter extends AbstractConverter {
 		List<RelationshipOccurence> relationshipOccurences = new ArrayList<RelationshipOccurence>();
 		//get ObjectOccurences
 		for (PetrinetNode node : source.getNodes()) {
-			objectOccurences.add(new ObjectOccurence(node.getLocalID().toString(), node.getClass().getSimpleName(), node.getLabel(), getElementCoordinates(node,layout).getX(), getElementCoordinates(node, layout).getY(), 0));
-//			objectOccurences.add(new ObjectOccurence(""+node.hashCode(), node.getClass().getSimpleName(), node.getLabel(), getElementCoordinates(node,layout).getX(), getElementCoordinates(node, layout).getY(), 0));
+//			objectOccurences.add(new ObjectOccurence(node.getLocalID().toString(), node.getClass().getSimpleName(), node.getLabel(), getElementCoordinates(node,layout).getX(), getElementCoordinates(node, layout).getY(), 0));
+			objectOccurences.add(new ObjectOccurence(""+node.hashCode(), node.getClass().getSimpleName(), node.getLabel(), getElementCoordinates(node,layout).getX(), getElementCoordinates(node, layout).getY(), 0));
 
 		}
 		//get RelationshipOccurences
 		for (PetrinetEdge edge : source.getEdges()) {
 			//Typecast to of source and target node to PetrinetNode to get ID
-			PetrinetNode sourceNode = (PetrinetNode) edge.getSource();
-			PetrinetNode targetNode = (PetrinetNode) edge.getTarget();
-			relationshipOccurences.add(new RelationshipOccurence(edge.getClass().getSimpleName(), sourceNode.getLocalID().toString(), targetNode.getLocalID().toString()));
-//			relationshipOccurences.add(new RelationshipOccurence(edge.getClass().getSimpleName(), ""+edge.getSource().hashCode(), ""+edge.getTarget().hashCode()));
+//			PetrinetNode sourceNode = (PetrinetNode) edge.getSource();
+//			PetrinetNode targetNode = (PetrinetNode) edge.getTarget();
+//			relationshipOccusrences.add(new RelationshipOccurence(edge.getClass().getSimpleName(), sourceNode.getLocalID().toString(), targetNode.getLocalID().toString()));
+			relationshipOccurences.add(new RelationshipOccurence(edge.getClass().getSimpleName(), ""+edge.getSource().hashCode(), ""+edge.getTarget().hashCode()));
 		}
 		Model modelToReturn = new Model(source.getLabel(), source.getClass().getSimpleName(), objectOccurences, relationshipOccurences); 
 		return modelToReturn;
